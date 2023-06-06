f321e2d890ae019fb17bda804996648972f21838
EtherSol <kylemz@arizona.edu>
2023-06-06 15:38:26 -0700

    success

--- ci_cd/play_action/Startup.cs
+++ ci_cd/play_action/Startup.cs
@@ -21,7 +21,7 @@ namespace play_action
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            // loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
             {
